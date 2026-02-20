62cd4593b02cf4d29c42908634678d6719d3e230
Phattraporn-bjk <phattraporn1565@gmail.com>
2026-02-20 16:20:42 +0700

    Merge origin/main into FirstpageCustomerLogin

--- node-api/db.js
+++ node-api/db.js
@@ -2,11 +2,7 @@ const mysql = require("mysql2");
 
 const Conn = mysql.createPool({
   host: process.env.DB_HOST,
-<<<<<<< HEAD
   port: process.env.DB_PORT,
-=======
-  port: process.env.DB_PORT,
->>>>>>> origin/main
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
