46c1a5b4b4c0ebdd97b0a05f580d2df2d2d3b7dd
KittimaM <riutyjku@gmail.com>
2026-02-20 14:57:51 +0700

    protected route

--- customer-web/src/App.js
+++ customer-web/src/App.js
@@ -4,11 +4,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CustomerIndex from "./Layouts/CustomerIndex";
 import CustomerLogin from "./Layouts/CustomerLogin";
 import CustomerRegister from "./Layouts/CustomerRegister";
+import ProtectedRoute from "./Layouts/ProtectedRoute";
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/customer/main" element={<CustomerIndex />} />
+        <Route
+          path="/customer/main"
+          element={
+            <ProtectedRoute>
+              <CustomerIndex />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/customer/register" element={<CustomerRegister />} />
         <Route path="/customer/login" element={<CustomerLogin />} />
         <Route path="/" element={<CustomerLogin />} />
